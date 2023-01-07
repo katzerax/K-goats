4342825a514fa36462a757ad35f17a60c884eed5
K <65287118+katzerax@users.noreply.github.com>
2023-01-07 13:38:33 -0800

    Update K's Goats.user.js

--- K's Goats.user.js
+++ K's Goats.user.js
@@ -1,6 +1,5 @@
 // ==UserScript==
 // @name         K's Goats
-// @namespace    idk lol
 // @version      0.1
 // @description  Increasing goatlings functionality
 // @author       K
@@ -15,9 +14,6 @@
 // @grant         GM_xmlhttpRequest
 // @require       https://code.jquery.com/jquery-3.5.1.min.js
 // ==/UserScript==
-/*--- Create a button in a container div.  It will be styled and
-    positioned with CSS.
-*/
 
 // Get the current URL of the page
 var currentURL = window.location.href;
@@ -28,4 +24,29 @@ if (currentURL.startsWith('https://www.goatlings.com/shops/')) {
   var result = value * 0.8;
   var inputBox = document.querySelector("#content > center > form > input[type=text]:nth-child(2)");
   inputBox.value = result;
+}
+else if(currentURL.startsWith('https://www.goatlings.com/ss_mine')){ //I DONT KNOW IF THIS WORKS YET
+  // Find all the elements on the page that contain numbers
+  var elements = document.querySelectorAll('.ss-area');
+  var numbers = [];
+  elements.forEach(function(element) {
+    numbers.push(parseInt(element.textContent, 10));
+  });
+
+// Find the highest number
+  var maxNumber = Math.max.apply(null, numbers);
+
+// Find the element that contains the highest number
+  var maxElement;
+  elements.forEach(function(element) {
+    if (parseInt(element.textContent, 10) === maxNumber) {
+      maxElement = element;
+    }
+  });
+
+// Find the img element above the element
+  var img = maxElement.previousElementSibling;
+
+// Click the img element
+  img.click();
 }
